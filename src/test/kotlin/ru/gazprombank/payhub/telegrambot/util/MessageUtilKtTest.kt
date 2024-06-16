0630e3f88a3ec5f03fab7467cf0ae338b595576d
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2024-06-16 14:34:15 +0300

    Создан тест MessageUtilKtTest

---
+++ src/test/kotlin/ru/gazprombank/payhub/telegrambot/util/MessageUtilKtTest.kt
@@ -0,0 +1,25 @@
+package ru.gazprombank.payhub.telegrambot.util
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.telegram.telegrambots.meta.api.methods.ParseMode
+import org.junit.jupiter.api.DisplayName
+import ru.gazprombank.payhub.telegrambot.util.*
+
+class MessageUtilKtTest {
+    @Test
+    @DisplayName("Корректность созданного SendMessage")
+    fun `testCreateMessage should create SendMessage with correct parameters`(){
+        val chatId = "12345"
+        val text = "Test message"
+
+        val message = createMessage(chatId, text)
+
+        assertNotNull(message)
+        assertEquals(chatId, message.chatId)
+        assertEquals(text, message.text)
+        assertEquals(ParseMode.MARKDOWN, message.parseMode)
+        assertTrue(message.disableWebPagePreview)
+    }
+}
+
