90058c4bf49f799f3b435371f1fd341ab2e670e0
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2024-06-23 12:56:01 +0300

    Исправлен RegisterCommandIntegrationTest

--- src/test/java/ru/gazprombank/payhub/telegrambot/client/RegisterCommandIntegrationTest.java
+++ src/test/java/ru/gazprombank/payhub/telegrambot/client/RegisterCommandIntegrationTest.java
@@ -10,38 +10,53 @@ import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.api.objects.User;
 import ru.gazprombank.payhub.telegrambot.command.RegisterCommand;
 
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 @AutoConfigureWireMock(port = 9090)
 class RegisterCommandIntegrationTest {
     @Autowired
     private RegisterCommand registerCommand;
-    private final User user = new User();
-    private final Chat chat = new Chat();
-    private final AbsSender absSender = spy(AbsSender.class);
+    private final AbsSender absSender = mock(AbsSender.class);
 
     @Test
     void testSuccessfulRegistration() {
+        final Long userId = 12345L;
+        final String userName = "testUserName";
+        final boolean isBot = false;
+        final User user = createTelegramUser(userId, userName, isBot);
+        final Chat chat = createChat(54321L);
         WireMock.stubFor(
                 WireMock.post(WireMock.urlEqualTo("/api/v1/users"))
                         .willReturn(WireMock.aResponse().withStatus(200).withBody("Регистрация успешна!"))
         );
-        prepareUserData(false);
 
         registerCommand.execute(absSender, user, chat, new String[]{});
 
         WireMock.verify(
                 WireMock.postRequestedFor(WireMock.urlEqualTo("/api/v1/users"))
-                        .withRequestBody(WireMock.equalToJson("{\"userId\":12345,\"userName\":\"testUserName\"}"))
+                        .withRequestBody(WireMock.equalToJson(String.format(
+                                """
+                                        {"userId":%d,
+                                        "userName":"%s"}""
+                                        """,
+                                userId,
+                                userName)))
         );
     }
 
-    private void prepareUserData(boolean isBot) {
-        user.setId(12345L);
-        user.setUserName("testUserName");
+    private Chat createChat(Long chatId) {
+        final Chat chat = new Chat();
+        chat.setId(chatId);
+        return chat;
+    }
+
+    private User createTelegramUser(Long userId, String userName, boolean isBot) {
+        final User user = new User();
+        user.setId(userId);
+        user.setUserName(userName);
         user.setIsBot(isBot);
-        chat.setId(54321L);
+        return user;
     }
 }
 
